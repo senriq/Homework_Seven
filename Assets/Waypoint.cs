cc98b404745b1bb6cdf039d36df92b1849de3891
senriq <svenriq07@gmail.com>
2024-11-25 15:01:56 -0500

    Initial commit

---
+++ Assets/Waypoint.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    [SerializeField] bool isPlaceable;
+
+    [SerializeField] GameObject towerPrefab;
+    private void OnMouseDown() {
+        if(isPlaceable){
+            Instantiate(towerPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
+
